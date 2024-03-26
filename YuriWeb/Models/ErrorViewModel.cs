548fb92d14dcc8168f62fcdf5197e9328d901712
Yuri Hong <YuriH@fpt.com>
2024-03-26 09:51:32 +0700

    Add project files.

---
+++ YuriWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YuriWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
