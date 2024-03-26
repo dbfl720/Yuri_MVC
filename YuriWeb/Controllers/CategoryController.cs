be43f9eb6298e2870ed7c770ce6163d4da1ef009
Yuri Hong <YuriH@fpt.com>
2024-03-26 23:02:03 +0700

    Section2 - C# CRUD

--- YuriWeb/Controllers/CategoryController.cs
+++ YuriWeb/Controllers/CategoryController.cs
@@ -23,5 +23,33 @@ namespace YuriWeb.Controllers
 			List<Category> objCategoryList = _db.Categories.ToList();
 			return View(objCategoryList);
 		}
+
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+
+		//  Entity framework core 
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+			}
+
+
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj); // keeping track of what it has to add
+				_db.SaveChanges(); // actually go to the database and create that category 
+				return RedirectToAction("Index"); // RedirectionToAction을 INDEX로 한 이유: ADD한 것을 INDEX메소드에서 DB에 RELOAD해야하니깐.
+
+			}
+			return View();
+		}
 	}
 }
--- YuriWeb/Models/Category.cs
+++ YuriWeb/Models/Category.cs
@@ -1,14 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace YuriWeb.Models
 {
-    public class Category
-    {
-        //1.
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        public int DisplayOrder { get; set; }
-    }
+	public class Category
+	{
+		//1.
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		[MaxLength(30)]
+		[DisplayName("Category Name")]
+		public string Name { get; set; }
+		[DisplayName("Display Order")]
+		[Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+		public int DisplayOrder { get; set; }
+	}
 }
